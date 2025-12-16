e9924af60b5b54b2b303d68a52d26f8ff1111727
WeatherheadOnline <WeatherheadOnline@gmail.com>
2025-12-16 20:42:36 +1100

    Added meta and og tags. Added some basic semantic structure in React files.

---
+++ littlelemon/src/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src="./images/image-footer.jpg" />
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Menu</a></li>
+        <li><a href="">Reservations</a></li>
+        <li><a href="">Order Online</a></li>
+        <li><a href="">Log in</a></li>
+      </ul>
+      <ul>
+        <li><h3>Contact</h3></li>
+        <li>Address line 1</li>
+        <li>Address line 2</li>
+        <li>phone number</li>
+        <li><email>email address</email></li>
+      </ul>
+      <ul>
+        <li><h3>Social Media</h3></li>
+        <li>Social media link</li>
+        <li>Social media link</li>
+        <li>Social media link</li>
+      </ul>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ littlelemon/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './Nav';
+
+const Header = () => {
+  return (
+    <>
+      <img src="./images/Logo.svg" />
+      <Nav />
+    </>
+  );
+};
+
+export default Header;
